3e0abbea9187b15b5bd1079cf63d2cb26b0a4d1e
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-06-19 13:27:03 -0400

    stuck

--- bash.js
+++ bash.js
@@ -1,9 +1,12 @@
+const pwd = require('./pwd')
+
+
 process.stdout.write('prompt > ');
 process.stdin.on('data', (x) => {
 	const cmd = x.toString().trim();
-	const cwd = process.cwd()
 	if (cmd === 'pwd') {
-		process.stdout.write(cwd)
+		process.stdout.write(`${pwd.pwder}`)
 	} 
+	else {process.stdout.write(cmd)};
 	process.stdout.write('\nprompt > ');
 });
---
+++ pwd.js
@@ -0,0 +1,5 @@
+function pwder() {
+	return process.stdout.write(process.cwd())
+}
+ 
+module.exports = pwder
